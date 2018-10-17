2bc1b31cf92a828407c4482bc5dd9f097701f5da
joenathan <cyborg.sasuke92@gmail.com>
2018-10-17 11:36:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ukrida/recycleviewmaterial/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.ukrida.recycleviewmaterial;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Herdi_WORK on 15.09.16.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataSet;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        dataSet = new ArrayList<>();
+        initDataset();
+
+        rvView = (RecyclerView) findViewById(R.id.rv_main);
+        rvView.setHasFixedSize(true);
+
+        /**
+         * Kita menggunakan LinearLayoutManager untuk list standar
+         * yang hanya berisi daftar item
+         * disusun dari atas ke bawah
+         */
+        layoutManager = new LinearLayoutManager(this);
+        rvView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerViewAdapter(dataSet);
+        rvView.setAdapter(adapter);
+
+    }
+
+    private void initDataset(){
+
+        /**
+         * Tambahkan item ke dataset
+         * dalam prakteknya bisa bermacam2
+         * tidak hanya String seperti di kasus ini
+         */
+        dataSet.add("Karin");
+        dataSet.add("Ingrid");
+        dataSet.add("Helga");
+        dataSet.add("Renate");
+        dataSet.add("Elke");
+        dataSet.add("Ursula");
+        dataSet.add("Erika");
+        dataSet.add("Christa");
+        dataSet.add("Gisela");
+        dataSet.add("Monika");
+
+    }
+}
